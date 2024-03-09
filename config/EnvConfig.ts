1cd2b201bc0ebdf4e9aced439cf2cf52dee2cb61
sbb-python <49127202+sbb-python@users.noreply.github.com>
2024-03-09 14:09:22 -0500

    Update EnvConfig.ts

--- config/EnvConfig.ts
+++ config/EnvConfig.ts
@@ -2,8 +2,8 @@ export const EnvConfig = {
     SIT: 'https://lplfinancial.wd1.myworkdayjobs.com/External',
     UAT: '',
     MS: 'https://reqres.in',
-    username: 'sandip.bhosale@outlook.com',
-    password: 'U2FsdGVkX19PBxrV8uNHsLfi3uTktcBagrO7rE3/SWU=',
+    username: '',
+    password: '',
     waitForElement: 120000,
   
 }
